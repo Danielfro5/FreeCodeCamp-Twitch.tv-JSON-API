ed454dbc671342c9d2deae4948615d2e7040c610
Danielfro5 <br3akcor3@yandex.com>
2017-03-26 18:43:09 +1100

    created API function that displays channel info

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 $(function() {
-    $.getJSON("https://wind-bow.gomix.me/twitch-api/users/noobs2ninjas?callback=?",function(twitchData){
-      console.log(twitchData);
+ twitchCall("opl","testId");
+ //twitchCall("saltybet","testId");
+ //twitchCall("redbull","testId");
 
-
-  });
 });
+
+
+function twitchCall(channelName, elementId){
+  var url = ("https://wind-bow.gomix.me/twitch-api/streams/"+channelName+"?callback=?");
+  $.getJSON(url,function(twitchData){
+    if(twitchData.stream === null){
+      document.getElementById(elementId).innerHTML = ("Channel Offline");
+      console.log("https://i.vimeocdn.com/portrait/10159238_300x300");
+    }
+    else{
+      document.getElementById(elementId).innerHTML = ("Channel Online<br>"+twitchData.stream.channel.status);
+      console.log(twitchData.stream.channel.logo);//logo image
+      
+    }
+    console.log(twitchData);
+    console.log("https://twitch.tv/"+channelName);
+
+});
+}
