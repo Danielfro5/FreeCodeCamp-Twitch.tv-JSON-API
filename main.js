8daec17ff48d11ecffb9640b72f4a17affc0d58d
Danielfro5 <br3akcor3@yandex.com>
2017-03-27 08:59:59 +1100

    added check if user exists, fully operational. need to remove global variable

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 var twitchStreamers = ["FreeCodeCamp","OPL","SaltyBet","RedBull","brunofin"];
+var userCallVar;
 
 $(function() {
- twitchClick();
-
+  twitchClick();
 
 });
+function userCall(channelName){
+  var urlUser = ("https://wind-bow.gomix.me/twitch-api/users/"+channelName+"?callback=?");
+  $.getJSON(urlUser,function(userData){
 
+    if(userData.status == "404"){
+      //variable not working
+      userCallVar = "User does not exist";
+
+    }else userCallVar =  "";
+
+  });
+
+}
 
 function twitchCall(channelName){
+
   var url = ("https://wind-bow.gomix.me/twitch-api/streams/"+channelName+"?callback=?");
+  userCall(channelName);
   $.getJSON(url,function(twitchData){
+
     if(twitchData.stream === null){
-      document.getElementById("mainDiv").innerHTML += ("<a href='https://twitch.tv/"+channelName+"'><img alt='channel logo' width='40' height='40' src='https://i.vimeocdn.com/portrait/10159238_300x300'><b>"+channelName+"</b> || Channel Offline ||</a><br>");
+      document.getElementById("mainDiv").innerHTML += ("<a href='https://twitch.tv/"+channelName+"'><img alt='channel logo' width='40' height='40' src='https://i.vimeocdn.com/portrait/10159238_300x300'><b>"+channelName+"</b> || Channel Offline ||"+userCallVar+"</a><br>");
 
     }
     else{
@@ -20,8 +35,7 @@ function twitchCall(channelName){
 
 
     }
-    console.log(twitchData);
-    console.log();
+
 
 });
 }
@@ -58,17 +72,23 @@ function twitchClick(){
   document.getElementById("mainDiv").innerHTML = null;
   for(i=0;i<twitchStreamers.length;i++){
    twitchCall(twitchStreamers[i]);
+
   }
 
 }
 
 function onlineClick(){
     document.getElementById("mainDiv").innerHTML = null;
+    for(i=0;i<twitchStreamers.length;i++){
      onlineCall(twitchStreamers[i]);
+   }
+
 
 }
 
 function offlineClick(){
   document.getElementById("mainDiv").innerHTML = null;
+  for(i=0;i<twitchStreamers.length;i++){
   offlineCall(twitchStreamers[i]);
+}
 }
